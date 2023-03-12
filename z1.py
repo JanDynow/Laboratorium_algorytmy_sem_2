e8d0bc21d60dbaa68576dfa131fdef48b0b1e4a4
JanDynow <116270007+JanDynow@users.noreply.github.com>
2023-03-12 20:04:29 +0100

    Delete z1.py

--- z1.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Podaj ilość liczb do wczytania: "))
-count = 0
-
-for i in range(1, n+1):
-    x = int(input(f"Podaj {i}. liczbę całkowitą: "))
-    if x < 0:
-        count += 1
-
-print(f"Ilość liczb ujemnych w ciągu wynosi: {count}")
